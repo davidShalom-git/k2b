19ecd00eb43a110d2fa31ec8bf784c39d3f4565e
davidshalom-git <davidshalom2609@gmail.com>
2025-06-23 21:02:48 +0530

    k2b

--- Backend/router/Waiter.js
+++ Backend/router/Waiter.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
-
-const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const Waiter = require('../models/Waiter.js')
 
 router.post('/register', async(req,res)=> {
